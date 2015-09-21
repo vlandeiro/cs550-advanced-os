fd76ef99aecc17631b4dda19a51fa6aded594fca
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-09-21 15:31:37 -0500

    update script to create 10 files instead of 5

--- PA1/p2p/files/create_files.sh
+++ PA1/p2p/files/create_files.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mkdir -p ${2}
-for i in 1 2 4 8 16;
-do dd if=/dev/urandom of=${2}/file_${i}K_${1} bs=1K count=$i;
+for k in 1 2;
+do for i in 1 2 4 8 16;
+   do dd if=/dev/urandom of=${2}/${i}K${1}${k} bs=1K count=$i;
+   done
 done
