6a996183cf596382bacdc8dd8ac5a1f4c58f4afc
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-10-13 02:15:43 +0000

    add debug messages

--- PA2/dht/dht_client.py
+++ PA2/dht/dht_client.py
@@ -58,6 +58,8 @@ class DHTClient(Process):
                         self.logger.error(repr(t))
             except KeyboardInterrupt as e:
                 sys.stderr.write("\r\n")
+            except EOFError as e:
+                self.dht.terminate.value = 1
 
     def _get_peer_sock(self, server_id):
         if server_id not in self.socket_map:
@@ -67,20 +69,24 @@ class DHTClient(Process):
             self.socket_map[server_id] = sock
         return self.socket_map[server_id]
 
-    def _generic_action(self, action, key, args):
+    def _generic_action(self, action, key, args, print_output=True):
         # hash key to get the server id
         server_id = self.dht.server_hash(key)
         # if local call parent, else call or connect to server
         if server_id == self.dht.id:
+            self.logger.debug("local %s" % (action))
             method = getattr(self.dht, action)
             res = method(*args)
         else:
+            self.logger.debug("network %s" % (action))
             sock = self._get_peer_sock(server_id)
             exch = MessageExchanger(sock)
             exch.send("%s %s" % (action, " ".join(args)))
             res = exch.recv()
             if res in str2py:
                 res = str2py[res]
+        if print_output:
+            print("RET> %s" % res)
         return False, res
 
     def _benchmark(self, action, first_key, count):
@@ -89,20 +95,23 @@ class DHTClient(Process):
         count = int(count)
         results = []
         t0 = time.time()
-        for k in range(first_key, first_key+count):
+        R = range(first_key, first_key+count)
+        for k in R:
             k = str(k)
             if action == "put":
-                args = [k, self.data[k]]
+                args = [k, self.data[k], False]
             else:
-                args = [k]
+                args = [k, False]
             func = getattr(self, "_" + action)
             _, ret = func(*args)
             results.append(ret)
         t1 = time.time()
         delta = t1 - t0
-        self.logger.info("%d %s operations completed in %.3f seconds." % (count, action, delta))
-        results = [True if i != None and i != False else False for i in results]
+        self.logger.info("Results check:")
+        if action == 'get':
+            results = [True if results[i] == self.data[str(k)] else False for i, k in enumerate(R)]
         self.logger.info("Results: %s" % (repr(Counter(results).most_common(2))))
+        self.logger.info("%d %s operations completed in %.3f seconds." % (count, action, delta))
         return False, None
 
     
