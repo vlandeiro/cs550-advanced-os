8b401094e5cffbf474dc9d832ca6176a1d30de1f
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-10-27 19:46:23 -0500

    move pa2 to pa3 folder

---
+++ PA3/dht/dht.py
@@ -0,0 +1,85 @@
+import json
+import sys
+import logging
+import os
+import time
+
+from urllib2 import urlopen
+from hashlib import md5
+from dht_client import DHTClient
+from dht_server import DHTServer
+from multiprocessing import Manager, Value, Lock
+
+logging.basicConfig(level=logging.DEBUG)
+
+class DHT:
+    def __init__(self, peers_map):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.peers_map = peers_map
+        self.peers_count = len(peers_map)
+
+        # get this server info from config file
+        this_ip = str(json.load(urlopen('https://api.ipify.org/?format=json'))['ip'])
+        this_server = filter(lambda s: s[1]['ip'] == this_ip, self.peers_map.iteritems())
+        if not this_server:
+            raise ValueError("peer %s is not included in the config file." % this_ip)
+        self.id, config = this_server[0]
+        self.ip = config['ip']
+        self.port = config['port']
+
+        self.manager = Manager()
+        self.hashmap = self.manager.dict()
+        self.map_lock = self.manager.Lock()
+
+        self.terminate = Value('i', 0)
+
+        self.server = DHTServer(self)
+        self.client = DHTClient(self)
+
+    def put(self, key, value):
+        """Fill the hashmap with a key and value."""
+        self.map_lock.acquire()
+        self.hashmap[key] = value
+        self.map_lock.release()
+        return True
+
+    def get(self, key):
+        """Get value of a given key."""
+        self.map_lock.acquire()
+        val = self.hashmap.get(key)
+        self.map_lock.release()
+        return val
+
+    def rem(self, key):
+        """Delete entry in the hashmap."""
+        self.map_lock.acquire()
+        del self.hashmap[key]
+        self.map_lock.release()
+        return True
+
+    def server_hash(self, key):
+        """Return the peer id to contact given a key."""
+        return int(md5(key).hexdigest(), 16) % self.peers_count
+
+
+def print_usage(args):
+    sys.stderr.write("Usage: python %s config.json\n" % args[0])
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 2:
+        print_usage(args)
+        sys.exit(1)
+    with open(args[1], 'r') as config_fd:
+        peers_map = json.load(config_fd)
+        peers_map = {int(id): peers_map[id] for id in peers_map}
+    dht_node = DHT(peers_map)
+    try:
+        dht_node.server.start()
+        time.sleep(1)
+        dht_node.client.run()
+        dht_node.server.join()
+    except KeyboardInterrupt as e:
+        sys.stderr.write("KeyboardInterrupt")
---
+++ PA3/dht/dht_server.py
@@ -0,0 +1,107 @@
+import json
+import logging
+import sys
+
+from select import select
+from dht_protocol import *
+from multiprocessing import Process
+from socket import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+class DHTServer(Process):
+    def __init__(self, dht):
+        super(DHTServer, self).__init__()
+
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
+
+        self.dht = dht
+        self.listening_socket = None
+        
+        self.actions_list = {
+            "put": self._put,
+            "get": self._get,
+            "rem": self._del
+        }
+        self.socket_list = []
+
+    def _put(self, key, value):
+        self.logger.debug("put")
+        self.dht.put(key, value)
+        return True
+
+    def _get(self, key):
+        self.logger.debug("get")
+        return self.dht.get(key)
+
+    def _del(self, key):
+        self.logger.debug("del")
+        self.dht.rem(key)
+        return True
+    
+    def _message_handler(self, sock, addr):
+        try:
+            sock.setblocking(0)
+            self.socket_list.append(sock)
+            
+            exch = MessageExchanger(sock)
+
+            while True:
+                try:
+                    readable, _, _ = select([sock], [], [], 0.1)
+                except error:
+                    sock.shutdown(2)
+                    sock.close()
+                    self.socket_list.remove(sock)
+                    break
+                if readable:
+                    cmd = exch.recv()
+                    if cmd is None:
+                        continue
+                    cmd_vec = cmd.split()
+                    action = cmd_vec[0]
+                    args = cmd_vec[1:]
+                    self.logger.debug("Request %s received." % (cmd))
+
+                    if action not in self.actions_list:
+                        exch.send(py2str[None])
+                    else:
+                        res = self.actions_list[action](*args)
+                        if res in py2str:
+                            exch.send(py2str[res])
+                        else:
+                            exch.send(res)
+                if self.dht.terminate.value == 1:
+                    break
+        except KeyboardInterrupt as e:
+            self.dht.terminate.value = 1
+    
+    def run(self):
+        self.logger.info('Starting DHT server.')
+        self.listening_socket = socket(AF_INET, SOCK_STREAM)
+        self.listening_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.listening_socket.bind(("0.0.0.0", self.dht.port))
+        self.listening_socket.listen(self.dht.peers_count)
+        self.logger.info("DHT server listening on port %d." % (self.dht.port))
+
+        read_list = [self.listening_socket]
+        try:
+            while True:
+                readable, _, _ = select(read_list, [], [], 0.1)
+                if self.dht.terminate.value == 1:
+                    break
+                elif readable:
+                    in_sock, in_addr = self.listening_socket.accept()
+                    handler = Process(target=self._message_handler, args=(in_sock, in_addr))
+                    handler.daemon = True
+                    handler.start()
+        except KeyboardInterrupt:
+            sys.stderr.write("\r")
+            self.logger.debug("Shutting down DHT server.")
+            self.dht.terminate.value = 1
+        finally:
+            self.listening_socket.close()
+
+
+    
