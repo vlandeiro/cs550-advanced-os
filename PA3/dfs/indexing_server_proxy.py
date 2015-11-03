5fb55016962c6f67665b0c55cbc601462ef552c2
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-11-03 18:22:45 +0000

    add metadata replication

--- PA3/dfs/indexing_server_proxy.py
+++ PA3/dfs/indexing_server_proxy.py
@@ -1,9 +1,13 @@
 import abc
 import sys
+import errno
+import logging
 
 from hashlib import md5
 from CommunicationProtocol import *
 from socket import *
+
+logging.basicConfig(level=logging.DEBUG)
 
 class ISProxy():
     __metaclass__ = abc.ABCMeta
@@ -31,6 +35,8 @@ class ISProxy():
 class CentralizedISProxy(ISProxy):
     def __init__(self, sock):
         self.exch = MessageExchanger(sock)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
 
     def init_connection(self, id):
         idx_action = dict(type='init', id=id)
@@ -62,13 +68,20 @@ class DistributedISProxy(ISProxy):
     def __init__(self, parent):
         self.parent = parent
         self.socket_map = [None]*self.parent.nodes_count
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
 
     def get_peer_sock(self, server_id):
         if server_id not in self.socket_map:
             sock = socket(AF_INET, SOCK_STREAM)
             peer_ip = self.parent.nodes_list[server_id]
-            sock.connect((peer_ip, self.parent.port))
-            self.socket_map[server_id] = sock
+            try:
+                sock.connect((peer_ip, self.parent.port))
+                self.socket_map[server_id] = sock
+            except error as e:
+                if e.errno == errno.ECONNREFUSED: # peer is not online
+                    self.logger.debug("Peer %s seems to be offline.", server_id)
+                    self.socket_map[server_id] = False
         return self.socket_map[server_id]
 
     def init_connection(self, id):
@@ -82,6 +95,8 @@ class DistributedISProxy(ISProxy):
         for sid, ip in enumerate(self.parent.nodes_list):
             if ip != self.parent.ip:
                 sock = self.get_peer_sock(sid)
+                if not sock:
+                    continue
                 exch = MessageExchanger(sock)
                 dht_action = dict(action="keys", args=[])
                 exch.pkl_send(dht_action)
@@ -91,6 +106,8 @@ class DistributedISProxy(ISProxy):
 
     def register(self, id, name):
         previous_peers = self._get(name)
+        if previous_peers is False: # nodes are offline
+            return False
         if previous_peers is None:
             previous_peers = []
         previous_peers.append(id)
@@ -105,15 +122,20 @@ class DistributedISProxy(ISProxy):
         return available_peers
 
     def _generic_action(self, action, key, args):
-        sys.stderr.write("%s\n" % action)
         # hash key to get the server id
         server_id = self.parent.server_hash(key)
+        return self._generic_action_sid(server_id, action, key, args)
+
+    def _generic_action_sid(self, sid, action, args):
+        sys.stderr.write("%s\n" % action)
         # if local call parent, else call or connect to server
         if server_id == self.parent.id:
             method = getattr(self.parent, action)
             res = method(*args)
         else:
             sock = self.get_peer_sock(server_id)
+            if sock is None:
+                return False
             exch = MessageExchanger(sock)
             dht_action = dict(action=action, args=args)
             exch.pkl_send(dht_action)
@@ -121,10 +143,24 @@ class DistributedISProxy(ISProxy):
         return res
 
     def _put(self, key, value):
-        return self._generic_action("put", key, [key, value])
+        sid = self.parent.server_hash(key)
+        sid_replica = (sid + 1) % self.parent.nodes_count
+        self._generic_action_sid(sid_replica, "put", [key, value])
+        return self._generic_action_sid(sid, "put", [key, value])
 
     def _get(self, key):
-        return self._generic_action("get", key, [key])
+        sid = self.parent.server_hash(key)
+        sid_replica = (sid + 1) % self.parent.nodes_count
+        sids = [sid, sid_replica]
+        obtained = False
+        while not obtained and sids:
+            sid = sids.pop(0)
+            obtained = self._generic_action_sid(sid,"get", [key])
 
     def _del(self, key):
-        return self._generic_action("rem", key, [key])
+        sid = self.parent.server_hash(key)
+        sid_replica = (sid + 1) % self.parent.nodes_count
+        sids = [sid, sid_replica]
+        for sid in sids:
+            self._generic_action_sid(sid, "rem", [key])
+        return True
