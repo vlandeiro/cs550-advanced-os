21a4b14525d19d15916d0a26ccca2e374c5a333a
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-11-05 22:53:07 -0600

    fix run.py

--- PA3/run.py
+++ PA3/run.py
@@ -3,12 +3,15 @@ import json
 from subprocess import call
 
 if __name__ == '__main__':
-    config_path = 'dfs/config.json'
-    with open(config_path) as config_fd:
-        config = json.load(config_fd)
-    cd_cmd = 'cd dfs;'
-    if 'idx_type' not in config: # centralized indexing server
-        cmd = 'python central_indexing_server.py config.json'
-    else: # peer
-        cmd = 'python node.py config.json'
-    call(cd_cmd + cmd, shell=True)
+    try:
+        config_path = 'dfs/config.json'
+        with open(config_path) as config_fd:
+            config = json.load(config_fd)
+        cd_cmd = 'cd dfs;'
+        if 'idx_type' not in config: # centralized indexing server
+            cmd = 'exec python central_indexing_server.py config.json'
+        else: # peer
+            cmd = 'exec python node.py config.json'
+        call(cd_cmd + cmd, shell=True)
+    except:
+        pass
