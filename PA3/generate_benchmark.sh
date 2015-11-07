3ea66c005de00367ba84e9bc26f1fe6a0bb944fc
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-11-07 05:20:55 +0000

    add ssd support to generate benchmark files

--- PA3/generate_benchmark.sh
+++ PA3/generate_benchmark.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+rm -rf data
+sudo mkdir -p /mnt/data
+sudo chmod a+w /mnt/data
+mkdir -p /mnt/data/download
+mkdir -p /mnt/data/local
+ln -s /mnt/data/ ./data
 cd ec2
 python gen_files.py
