aab6089f1d0bea8d7cbb7a34e89d22d71dd0924f
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-11-20 15:33:53 -0600

    fix gen_data loop

---
+++ PA4/gen_data.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+KEYSIZE = 10
+VALUESIZE = 90
+
+charset_list = list(string.ascii_lowercase + string.ascii_uppercase + string.digits)
+l = len(charset_list)
+
+def gen_rand_string(size):
+    return "".join([charset_list[int(random.random()*l)] for _ in range(size)])
+
+def gen_key_value(count):
+    key_vals = {gen_rand_string(10): gen_rand_string(90) for _ in range(count)}
+    # loop will be true when the same random key has been generated twice: that
+    # is very unlikely!
+    while len(key_vals) < count:
+        key_vals[gen_rand_string(10)] = gen_rand_string(90)
