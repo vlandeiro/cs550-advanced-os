190cb1ab4616a7231b5b4103828992636033112f
Virgile Landeiro <vlandeir@hawk.iit.edu>
2015-11-29 14:41:22 -0600

    make worload unique in each node

--- PA4/workload.py
+++ PA4/workload.py
@@ -1,18 +1,24 @@
 import random
 import string
+import pycurl as pc
 
 KEYSIZE = 10
 VALUESIZE = 90
-SEED = 111191
+SEED_BASE = "111191"
 
 charset_list = list(string.ascii_lowercase + string.ascii_uppercase + string.digits)
 l = len(charset_list)
+
+def get_private_ip():
+    c = pc.Curl()
+    c.setopt(URL, "http://169.254.169.254/latest/meta-data/hostname")
+    return c.perform()
 
 def gen_rand_string(size):
     return "".join([charset_list[int(random.random()*l)] for _ in range(size)])
 
 def gen_workload(count):
-    random.seed(SEED) # same workload is generated every time
+    random.seed(SEED + get_private_ip()) # same workload is generated every time
     key_vals = {gen_rand_string(10): gen_rand_string(90) for _ in range(count)}
     # loop will be true when the same random key has been generated twice: that
     # is very unlikely!
